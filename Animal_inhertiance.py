4fc5c7e0949d1266c5289fec9d7addd69727b925
Sathyakiran151 <sathyakiran2498@gmail.com>
2025-10-27 22:47:36 +0530

    Initial commit - added Animal_inhertiance.py

---
+++ Animal_inhertiance.py
@@ -0,0 +1,27 @@
+'''Create a base class Animal with properties like name, age, and sound().
+
+Derive subclasses like Dog, Cat, Cow, each overriding sound().'''
+class Animal():
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def sound(self):
+        print("animal makes the sound")
+class Dog(Animal):
+    def sound(self):
+        print("bow bow")
+class Cat(Animal):
+    def sound(self):
+        print("meow meow")
+class Cow(Animal):
+    def sound(self):
+        print("amba amba")
+dog = Dog("tyson",3)
+cat= Cat("tommy",4)
+cow = Cow("chinni",5)
+dog.sound()
+cat.sound()
+cow.sound()
+        
+        
+    
